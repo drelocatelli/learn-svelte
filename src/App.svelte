226fc0399b3840dee342ae801d06421be7646d0b
Your Name01~ <you@example.com>
2022-06-09 22:48:56 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Greetings from './components/greetings.svelte';
+
+	export let name;
+
+	let age = 22;
+
+	const incrementAge = () => {
+		age += 1;
+	}
+</script>
+
+<main>
+	<h1>Hello {name}, my age is {age}</h1>
+	<button on:click={incrementAge}>Increment age</button>
+	<hr>
+	Greetings html: <br />
+	<Greetings name={name}>You're welcome!</Greetings>
+</main>
